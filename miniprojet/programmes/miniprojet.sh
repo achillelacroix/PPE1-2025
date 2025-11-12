1fe27b0d4b17fb4204d9dfa738df37c6c5d7de24
achillelacroix <achille.lacroix@sorbonne-nouvelle.fr>
2025-11-12 10:38:49 +0100

    ajout index.html + modif miniprojet

--- miniprojet/programmes/miniprojet.sh
+++ miniprojet/programmes/miniprojet.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 if [ $# -ne 2 ]; then
     echo "Ce programme demande deux arguments : 1 fichier d'entrée contenant des urls et 1 fichier sortie"
-    exit
+    exit 1
 fi
 
 FICHIER_URLS=$1
@@ -9,28 +9,44 @@ SORTIE=$2
 
 if [ ! -f "$FICHIER_URLS" ]; then
     echo "Ce programme demande un fichier"
-    exit
+    exit 1
 fi
 
 NB_LIGNE=0 # on aurait pu mettre lineno
 
-echo -e "<html>" > "$SORTIE"
-echo -e "\t<head>Ceci est une entête</head>" >> "$SORTIE"
-echo -e "\t<body>" >> "$SORTIE"
-echo -e "\t\t<table>" >> "$SORTIE"
-echo -e "\t<tr><th>Numéro</th><th>Adresse</th><th>CodeHttp</th><th>UTF8?</th><th>NbDeMots</th></tr>" >> "$SORTIE"
+echo -e "
+<html>
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <table>
+            <tr>
+                <th>Numéro</th>
+                <th>Adresse</th>
+                <th>CodeHttp</th>
+                <th>UTF8?</th>
+                <th>NbDeMots</th>
+            </tr>" > "$SORTIE"
 
 while read -r LINE ; do
     if [[ $LINE =~ ^https?:// ]]; then
 
         NB_LIGNE=$(expr $NB_LIGNE + 1)
 
-        CODE_ET_ENCODAGE=$(curl -s -L -i -o "tmp.txt" -w "%{http_code}\n%{content_type}" "$LINE")
+        CODE_ET_ENCODAGE=$(curl -s -L -i -o ".tmp.txt" -w "%{http_code}\n%{content_type}" "$LINE")
 
         CODE=$(echo "$CODE_ET_ENCODAGE" | head -n 1)
 
         if [ $CODE -eq 0 ]; then
-            echo -e "\t<tr>\n<td>$NB_LIGNE</td>\t<td>$LINE</td>\t<td>ERREUR</td>\t<td>ERREUR</td>\t<td>ERREUR</td>\n</tr>" >> "$SORTIE"
+            echo -e "
+            <tr>
+                <td>$NB_LIGNE</td>
+                <td>$LINE</td>
+                <td>ERREUR</td>
+                <td>ERREUR</td>
+                <td>ERREUR</td>
+            </tr>" >> "$SORTIE"
             continue
         fi
 
@@ -42,14 +58,22 @@ while read -r LINE ; do
             ENCODAGE_OU_PAS="NON"
         fi
 
-        NB_MOTS=$(cat "tmp.txt" | lynx -dump -stdin -nolist | wc -w)
+        NB_MOTS=$(cat ".tmp.txt" | lynx -dump -stdin -nolist | wc -w)
 
-        rm "tmp.txt"
+        rm ".tmp.txt"
 
-        echo -e "\t<tr>\n<td>$NB_LIGNE</td>\t<td>$LINE</td>\t<td>$CODE</td>\t<td>$ENCODAGE_OU_PAS</td>\t<td>$NB_MOTS</td>\n</tr>" >> "$SORTIE"
+        echo -e "
+            <tr>
+                <td>$NB_LIGNE</td>
+                <td>$LINE</td>
+                <td>$CODE</td>
+                <td>$ENCODAGE_OU_PAS</td>
+                <td>$NB_MOTS</td>
+            </tr>" >> "$SORTIE"
 fi
 done  < "$FICHIER_URLS"
 
-echo -e "\t\t</table>" >> "$SORTIE"
-echo -e "\t</body>" >> "$SORTIE"
-echo -e "</html>" >> "$SORTIE"
+echo -e "
+        </table>
+    </body>
+</html>" >> "$SORTIE"
